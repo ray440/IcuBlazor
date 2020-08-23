e92e755872c778cf435a0add39ca5dd105fd4b49
Ray <ray@nxs7.net>
2020-08-23 18:45:14 -0400

    faster image comparison

--- src/IcuBlazor.Native.Win/XBitmap.fs
+++ src/IcuBlazor.Native.Win/XBitmap.fs
@@ -42,6 +42,8 @@ module private XBM =
                 currRow <- emptyRow
             currRow
 
+        member val Img = img
+
         member __.GetRow(r) =
             if (row <> r) then (get_row r) else currRow
 
@@ -71,15 +73,12 @@ module private XBM =
         let cb = (diff_hue s.B t.B)
         0xff000000 ||| cr ||| cg ||| cb
 
-    let diff_by_pixel(img0:BitmapSource, img1:BitmapSource) =
+    let make_image_diff(bm0:XBitmapReader, bm1:XBitmapReader) =
         
-        let maxw = Math.Max(img0.PixelWidth, img1.PixelWidth)
-        let maxh = Math.Max(img0.PixelHeight, img1.PixelHeight)
-        let bm0 = new XBitmapReader(img0)
-        let bm1 = new XBitmapReader(img1)
+        let maxw = Math.Max(bm0.Img.PixelWidth, bm1.Img.PixelWidth)
+        let maxh = Math.Max(bm0.Img.PixelHeight, bm1.Img.PixelHeight)
 
         let pixels = Array.zeroCreate<int32>(maxh*maxw)
-        let mutable diff = 0
         let mutable color = 0
         for r = 0 to (maxh-1) do
             let mutable ind = r*maxw
@@ -92,7 +91,6 @@ module private XBM =
                     let s = FromArgb(p0)
                     let t = FromArgb(p1)
                     if (far s.R t.R) || (far s.G t.G) || (far s.B t.B) || (far s.A t.A) then
-                        diff <- diff + 1
                         color <- diff_color s t
                     else
                         color <- 0xffffffff
@@ -100,33 +98,12 @@ module private XBM =
                 pixels.[ind] <- color
                 ind <- ind + 1
 
-        (diff, pixels, maxw, maxh)
+        let maxImg = if (bm0.Img.PixelWidth > bm1.Img.PixelWidth) 
+                     then bm0.Img else bm1.Img
+        let img = Create(maxImg, maxw, maxh, pixels)
+        img
 
-module XBitmap =
-    open XBM
-
-    [<PermissionSet(SecurityAction.Assert, Name="FullTrust")>] // FxCop: LinkDemand on Save()
-    let Save(fileName, image:BitmapSource) =
-        let enc = new PngBitmapEncoder()
-        enc.Frames.Add(BitmapFrame.Create(image))
-        use s = File.OpenWrite(fileName)
-        enc.Save(s)
-
-    let Load(fileName) =
-        try 
-            let b = new BitmapImage()
-            b.BeginInit()
-            // Turn cache off otherwise file will be locked!
-            b.CacheOption <- BitmapCacheOption.OnLoad
-            b.CreateOptions <- BitmapCreateOptions.IgnoreImageCache
-            b.UriSource <- new Uri(fileName, UriKind.RelativeOrAbsolute)
-            b.EndInit()
-            b
-        with 
-            :? FileNotFoundException ->
-            null
-
-    let FindPixels(image:BitmapSource, m:int32[]) =
+    let find_pixels(image:BitmapSource, m:int32[]) =
         let mlen = m.Length
         let m0 = m.[0]
         let (w, h) = (image.PixelWidth - mlen, image.PixelHeight)
@@ -150,11 +127,61 @@ module XBitmap =
             r <- r + 1
         found
 
+    let quickdiff_by_pixel(bm0:XBitmapReader, bm1:XBitmapReader) =
+        let (w, h) = (bm0.Img.PixelWidth, bm0.Img.PixelHeight)
+        let mutable diff = 0
+        let mutable r = 0
+        while (r < h) do
+            let mutable c = 0
+            while (c < w) do                
+                let p0 = bm0.GetPixel(r, c)
+                let p1 = bm1.GetPixel(r, c)
+                if (p0 <> p1) then
+                    diff <- 1
+                    c <- w
+                    r <- h
+                c <- c + 1
+            r <- r + 1
+        diff
+
+    let quickdiff(bm0:XBitmapReader, bm1:XBitmapReader) =
+        if (bm0.Img.PixelHeight<> bm1.Img.PixelHeight)
+         ||(bm0.Img.PixelWidth <> bm1.Img.PixelWidth) then
+            1
+        else
+            quickdiff_by_pixel(bm0, bm1)
+
+module XBitmap =
+    open XBM
+
+    let FindPixels(image, m) = find_pixels(image, m)
+
+    [<PermissionSet(SecurityAction.Assert, Name="FullTrust")>] // FxCop: LinkDemand on Save()
+    let Save(fileName, image:BitmapSource) =
+        let enc = new PngBitmapEncoder()
+        enc.Frames.Add(BitmapFrame.Create(image))
+        use s = File.OpenWrite(fileName)
+        enc.Save(s)
+
+    let Load(fileName) =
+        try 
+            let b = new BitmapImage()
+            b.BeginInit()
+            // Turn cache off otherwise file will be locked!
+            b.CacheOption <- BitmapCacheOption.OnLoad
+            b.CreateOptions <- BitmapCreateOptions.IgnoreImageCache
+            b.UriSource <- new Uri(fileName, UriKind.RelativeOrAbsolute)
+            b.EndInit()
+            b
+        with 
+            :? FileNotFoundException ->
+            null
+
     let ImageDiff (img0:BitmapSource) (img1:BitmapSource) (fname:string) =
-        let (diff, pixels, maxw, maxh) = diff_by_pixel(img0, img1)
-        if not(diff = 0 || String.IsNullOrEmpty(fname)) then
-            let bigger_img = 
-                if (img0.PixelWidth > img1.PixelWidth) then img0 else img1
-            let img = Create(bigger_img, maxw, maxh, pixels)
+        let bm0 = new XBitmapReader(img0)
+        let bm1 = new XBitmapReader(img1)
+        let diff = quickdiff(bm0, bm1)
+        if diff > 0 && not(String.IsNullOrEmpty(fname)) then
+            let img = make_image_diff(bm0, bm1)
             Save(fname, img)
         diff
