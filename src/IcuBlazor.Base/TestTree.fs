a699e2da4da7f9194f77ca061f98256e757b3465
Ray <ray@nxs7.net>
2020-11-04 20:01:45 -0500

    split out UI & Tree code

---
+++ src/IcuBlazor.Base/TestTree.fs
@@ -0,0 +1,153 @@
+﻿namespace IcuBlazor
+
+open System
+open IcuBlazor.Models
+
+module internal IcuCoreTree =
+
+    module ReflectTest =
+        open System.Reflection
+        open System.Threading.Tasks
+        open System.Runtime.ExceptionServices
+
+        let Create(typ:Type) = // create a type with an empty ctor()
+            typ.GetConstructor(Array.empty).Invoke(Array.empty)
+
+        [<System.Diagnostics.DebuggerNonUserCode>]
+        let Async_VoidMethod this (m:MethodInfo) args = async {
+            try 
+                m.Invoke(this, args) |> ignore 
+            with 
+            | :? TargetInvocationException as e ->
+                // All this to get actual exception...
+                ExceptionDispatchInfo.Capture(e.InnerException).Throw()
+        }
+        [<System.Diagnostics.DebuggerNonUserCode>]
+        let Async_TaskMethod this (m:MethodInfo) args = async { // Note: outer async is needed
+            do! m.Invoke(this, args) :?> Task |> Async.AwaitTask
+        }
+        [<System.Diagnostics.DebuggerNonUserCode>]
+        let Async_AsyncMethod this (m:MethodInfo) args = async {
+            do! m.Invoke(this, args) :?> Async<unit>
+        }
+
+        let TypeTask = typeof<Task>
+        let TypeAsync = typeof<Async<unit>>
+        let TypeVoid = typeof<Void>
+
+        [<System.Diagnostics.DebuggerNonUserCode>]
+        let ToAsyncMethod this (m:MethodInfo) arg =
+            let typ = m.ReturnType
+            let args = [| arg |]
+            if TypeVoid.Equals(typ) then    Async_VoidMethod this m args
+            elif TypeTask.Equals(typ) then  Async_TaskMethod this m args
+            elif TypeAsync.Equals(typ) then Async_AsyncMethod this m args
+            else
+                new IcuException(
+                    SF "Invalid return type %s.%s -> %A" m.DeclaringType.Name m.Name typ.Name,
+                    "1) Tests can only return void, Task or F# Async<unit>\n" )
+                |> raise
+
+        let private flags =             
+            BindingFlags.Instance
+            ||| BindingFlags.DeclaredOnly // No inherited methods.
+            ||| BindingFlags.Public
+            ||| BindingFlags.NonPublic
+        let DeclaredMethods (t:Type) = t.GetMethods(flags)
+
+        let mutable TestArgType = Type.Missing.GetType()
+
+        let MethodIsTestable (m:MethodInfo) =
+            let ps = m.GetParameters()
+            let n = m.Name
+            (ps.Length = 1)
+            && (ps.[0].ParameterType.Equals(TestArgType))
+            && (not m.IsConstructor)
+            && (not(n.[0] = '_' || n.StartsWith "SKIP"))
+
+        let DefaultTestsOf thisObj =
+            thisObj.GetType() 
+            |> DeclaredMethods 
+            |> Seq.filter MethodIsTestable
+            |> Seq.toArray
+            |> Array.sortBy (fun m -> m.MetadataToken)
+            //|> Array.sortBy (fun m -> m.Name)
+
+
+        let AllMethods asm = Reflect.AllTypes asm |> Seq.map(DeclaredMethods) |> Seq.concat
+
+        //let AllTestsWithAttr<'T> asm = 
+        //    let attrType = typeof<'T>
+        //    AllMethods asm
+        //    |> Seq.filter(WithAttr attrType)
+        //    |> Seq.filter(MethodIsTestable)
+
+
+    type internal TestTree(root:TestSuite, config:IcuConfig, msgBus:MessageBus) =
+
+        let tests = new CDict<string, TestMethod>()
+
+        let mutable topSuite = root.Path
+
+        let set_suite path = topSuite <- SlashPath.Make root.Path path
+
+        let is_runnable (tm:TestMethod) = 
+            tm.Name.ToLower().Contains(config.Filter)
+
+        let filtered_methods ts =
+            if config.Filter.Length > 0 
+            then ts |> Seq.filter is_runnable
+            else ts
+            |> ResizeArray
+
+        let tests_of thisObj =
+            tests.Dict()
+            |> Seq.map(fun kv -> kv.Value)
+            |> Seq.filter(fun t -> t.ThisFunc.This = thisObj)
+            |> filtered_methods
+
+        let fetch_suite testPath =
+            let path = testPath |> SlashPath.FromString
+            let n = path.Length
+            let mutable st = root
+            for i in 1..(n-2) do
+                st <- st.FetchSuite (path.[i])
+            st
+
+        let add_func thisFunc =
+            let testPath = SlashPath.Make topSuite thisFunc.Name
+            if not(tests.ContainsKey testPath) then
+                let suite = fetch_suite testPath
+                let tm = suite.AddMethod thisFunc 
+                tests.Set(testPath, tm)
+                msgBus.Notify(AddTest tm)
+
+        let add_test testName makeArg thisObj testf = 
+            { Name=testName; This=thisObj; Func=testf; MakeArg=makeArg }
+            |> add_func 
+
+        let as_ThisFunc makeArg thisObj m = {
+            This = thisObj
+            MakeArg = makeArg 
+            Func = ReflectTest.ToAsyncMethod thisObj m 
+            Name = Str.TitleCase m.Name
+        }
+
+        let add_default_tests makeArg (thisObj:obj) =
+            set_suite (thisObj.GetType().Name |> Str.TitleCase)
+
+            let toFunc = as_ThisFunc makeArg thisObj
+            ReflectTest.DefaultTestsOf thisObj
+            |> Seq.map toFunc
+            |> Seq.iter add_func
+
+        member val Root = root
+        member val Tests = tests
+        member val MsgBus = msgBus
+
+        member __.Suite path = set_suite path 
+        member __.Add = add_test 
+        member __.AddDefaultTests = add_default_tests
+        member __.FilteredMethods = filtered_methods
+        member __.TestsOf thisObj = tests_of thisObj
+
---
+++ src/IcuBlazor.Base/UImodels.fs
@@ -0,0 +1,145 @@
+﻿namespace IcuBlazor
+
+open System
+open System.Collections.Generic
+open Microsoft.FSharp.Core
+
+module DiffService =
+    open DiffPlex
+    open System.Text
+    
+    let diffBuilder = Differ() |> DiffBuilder.SideBySideDiffBuilder
+
+    let GetDiffs oldText newText =
+        diffBuilder.BuildDiffModel(oldText, newText, false)
+
+    let WSdiffs a b =
+        // Some white space diffs can't be seen on GUI. Detect and notify.
+        let stripws (s:string) = 
+            s |> Seq.exclude Char.IsWhiteSpace |> Seq.toArray |> String
+        let strip_equal() = 
+            String.Equals(stripws a, stripws b)
+
+        let same = String.Equals(a,b)
+        let wsdiff = (not same) && strip_equal()
+        (same, wsdiff)
+
+    let WSescape s =
+        let sb = new StringBuilder()
+        for c in s do
+            match c with
+            | '\r' -> sb.Append("\\r")
+            | '\n' -> sb.Append("\\n\n")
+            | '\t' -> sb.Append("\\t")
+            | _ -> sb.Append(c)
+            |> ignore
+        sb.ToString()
+        
+module Lorem =
+    open System.Text
+
+    let lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ";
+    let len = lorem.Length
+
+    let OfLength(n:int) = 
+        let sb = new StringBuilder(n)
+        for i in 1..(n/len) do
+            sb.Append(lorem) |> ignore
+        sb.Append(lorem.Substring(0, n % len)) |> ignore
+        sb.ToString()
+
+
+module BlazorUI =
+
+    let private get_attr (attrs:IDictionary<string, obj>) n = 
+        if attrs.ContainsKey(n) then attrs.[n].ToString() else ""
+
+    let private with_class attrs extraClass = 
+        let n = "class"
+        let v = get_attr attrs n
+        attrs.[n] <- extraClass + " " + v
+
+    let private with_style attrs extraStyle = 
+        let n = "style"
+        let v = get_attr attrs n
+        attrs.[n] <- v + ";" + extraStyle
+    
+    let AddAttrs dict extraClass extraStyle = 
+        let attrs = BaseUtils.CopyDict(dict)
+        with_class attrs extraClass
+        with_style attrs extraStyle
+        attrs
+
+    let private find_type : (Type*string) -> Option<Type> = 
+        let rec findr (t:Type, suffix:string) =
+            // Find matching type(+suffix). If not found move up type hierarchy.
+            match Reflect.FindClass(t.Name+suffix) with
+            | Some typ -> Some typ
+            | _ -> 
+                match t.BaseType with
+                | null -> None
+                | _ as bt -> findr(bt, suffix)
+
+        memoize findr
+       
+    let FindTypeWithSuffix model suffix defType =
+        match model with
+        | null -> defType
+        | _ -> 
+            match find_type(model.GetType(),suffix) with
+            | Some t -> t
+            | _ -> defType
+
+
+module SlashPath = 
+    let FromString = 
+        let toArray path = 
+            path |> Str.Split "/" |> Array.filter(fun p -> p.Length > 0)
+        memoize toArray
+
+    let Make (root:string) (node:string) = 
+        if root.EndsWith("/")
+        then root + node
+        else SF "%s/%s" root node
+
+module Tree =
+
+    type Base(parent:Option<Base>, name) =
+        let path = 
+            let root = if parent.IsSome then parent.Value.Path else ""
+            SlashPath.Make root name
+        let onChange = new Subscribable<obj>()
+        let _id = Guid.NewGuid().ToString()
+
+        member val ID = _id
+        member val Path = path
+        member val Name = name
+        member val OnChange = onChange
+        member val Open = false with get, set
+        member g.ToggleOpen() = g.Open <- not g.Open
+
+
+    type Node<'Kid>(parent, name) =
+        inherit Base(parent, name)
+
+        let kids = new CList<'Kid>();
+        let lookup = new CDict<string, 'Kid>();
+
+        member val Parent = parent
+        member val Kids = kids
+        member val Lookup = lookup
+
+        member __.Add key c =
+            kids.Add(c)
+            lookup.Set(key, c)
+    
+        member __.Clear() =
+            kids.Clear()
+            lookup.Clear()
+
+    let rec DepthFirstWalk(root:Node<_>) = seq {
+        for s in root.Kids.List() do
+            yield! (DepthFirstWalk s)
+        yield box root
+    }
+
